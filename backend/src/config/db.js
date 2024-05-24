610ba6d1cf28d4dfbf3ac1353b68eab110823e08
Wilson Seabra <wil@taprapeixe.com.br>
2024-05-23 21:16:35 -0300

    mongodb added

--- backend/src/config/db.js
+++ backend/src/config/db.js
@@ -2,8 +2,7 @@ import mongoose from 'mongoose';
 
 const connectDB = async () => {
   try {
-    await mongoose.connect('mongodb://localhost:27017/mongo-to-do', {
-    });
+    await mongoose.connect('mongodb://localhost:27017/mongo-to-do');
     console.log('Conectado ao MongoDB');
   } catch (err) {
     console.error('Erro ao conectar ao MongoDB', err);
