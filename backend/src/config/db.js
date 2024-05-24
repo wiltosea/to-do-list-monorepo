1470b90a673cc3bbeadfcc3705c8ae0ac3896a49
Wilson Seabra <wil@taprapeixe.com.br>
2024-05-24 19:58:03 -0300

    adicionada opção para remover item da lista

--- backend/src/config/db.js
+++ backend/src/config/db.js
@@ -2,7 +2,7 @@ import mongoose from 'mongoose';
 
 const connectDB = async () => {
   try {
-    await mongoose.connect('mongodb://localhost:27017/mongo-to-do');
+    await mongoose.connect('mongodb://127.0.0.1:27017/mongo-to-do');
     console.log('Conectado ao MongoDB');
   } catch (err) {
     console.error('Erro ao conectar ao MongoDB', err);
